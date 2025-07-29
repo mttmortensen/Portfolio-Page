f641cb7818eacb0f4a75da5ad9b2523952fc984f
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-29 15:49:11 -0600

    adding values to for a about model class but trying not to do a over kill on a static feature

--- Models/AboutData.cs
+++ Models/AboutData.cs
@@ -2,5 +2,13 @@
 {
     public class AboutData
     {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Bio { get; set; }
+        public string Location { get; set; }
+        public string PhotoURL { get; set; }
+        public string TagLine { get; set; }
+        public List<string> SkillsSummary { get; set; } = new List<string>();
+        public Dictionary<string, string> SocialLinks { get; set; } = new Dictionary<string, string>();
     }
 }
