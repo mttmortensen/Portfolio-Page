bd1a5d1b01f88016c214639ede1bb1ae9de63c3d
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-29 16:18:19 -0600

    Adding values for a project. Updating TechStack to it's own class

--- Models/ProjectModel.cs
+++ Models/ProjectModel.cs
@@ -2,5 +2,15 @@
 {
     public class ProjectModel
     {
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string LongDescription { get; set; }
+        public List<string> TechStack { get; set; } = [];
+        public string ImageURL { get; set; }
+        public string GHRepoURL { get; set; }
+        public string LiveURL { get; set; }
+        public bool IsFeatured { get; set; }
+        public DateTime DateCompleted { get; set; }
     }
 }
