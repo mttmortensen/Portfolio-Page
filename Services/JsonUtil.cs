519fb809ef9f81ee190c3adeb6b6272df61237bd
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-30 15:22:21 -0600

    A simple json tool that will read the json static data from Data

---
+++ Services/JsonUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace Portfolio_Page.Services
+{
+    public static class JsonUtil
+    {
+        public static T LoadJson<T>(string relativePath, string rootPath) 
+        {
+            var filePath = Path.Combine(rootPath, relativePath);
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"JSON File not found at {filePath}");
+
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<T>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        }
+    }
+}
