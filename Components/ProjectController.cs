dcb09e5c7d4e29d93eb6cd03e60d582f529ff207
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-29 23:02:29 -0600

    Simple controller for handling the req for projects

---
+++ Components/ProjectController.cs
@@ -0,0 +1,19 @@
+﻿using Portfolio_Page.Models;
+using Portfolio_Page.Services;
+
+namespace Portfolio_Page.Components
+{
+    public class ProjectController
+    {
+        private readonly ProjectServices _service;
+
+        public ProjectController(ProjectServices service)
+        {
+            _service = service;
+        }
+
+        public List<ProjectModel> GetAll() => _service.GetAllProjects();
+
+        public ProjectModel? GetBySlug(string slug) => _service.GetProjectBySlug(slug);
+    }
+}
