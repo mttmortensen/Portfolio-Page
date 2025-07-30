435acef774b4872e055974c450902cd4ae0dff6b
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-30 15:58:36 -0600

    getting appsettings and correct services setup in the program.cs and csproj

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Portfolio_Page.Components;
+using Portfolio_Page.Controllers;
 using Portfolio_Page.Services;
+using System.Net.Http;
 
 namespace Portfolio_Page
 {
@@ -11,14 +12,19 @@ namespace Portfolio_Page
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-            builder.Services.AddSingleton <ProjectServices>();
-            builder.Services.AddSingleton <ProjectController>();
-
+            // Register root components
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            // Register HttpClient as scoped (this is default in Blazor WASM)
+            builder.Services.AddScoped(sp => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+
+            // Register your services as SCOPED (not singleton!)
+            builder.Services.AddScoped<ProjectServices>();
+            builder.Services.AddScoped<ProjectController>(); // optional
 
             await builder.Build().RunAsync();
         }
