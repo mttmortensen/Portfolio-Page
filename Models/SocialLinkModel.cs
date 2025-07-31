5e2e6f11747715d080b7a57696ed6c5df8e645ce
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-31 11:08:32 -0600

    simple model for SL

---
+++ Models/SocialLinkModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Portfolio_Page.Models
+{
+    public class SocialLinkModel
+    {
+        public string Platform { get; set; }
+        public string URL { get; set; }
+        public string Icon { get; set; } // Bootstrap Icons
+    }
+}
