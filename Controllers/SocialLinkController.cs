8aaf0e2e2c3e1f2d25b6808fe6353d860ed8c9d3
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-31 11:27:25 -0600

    simple controller that the component will use

---
+++ Controllers/SocialLinkController.cs
@@ -0,0 +1,17 @@
+﻿using Portfolio_Page.Models;
+using Portfolio_Page.Services.Core;
+
+namespace Portfolio_Page.Controllers
+{
+    public class SocialLinkController
+    {
+        private readonly SocialLinkServices _service;
+
+        public SocialLinkController(SocialLinkServices service)
+        {
+            _service = service;
+        }
+
+        public async Task<List<SocialLinkModel>> GetAll() => await _service.GetAllLinks();
+    }
+}
