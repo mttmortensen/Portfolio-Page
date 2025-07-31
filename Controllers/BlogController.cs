e0ebad19ce882d1b70e1335319a3f9a4938f023c
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-31 09:35:00 -0600

    getting all those logs from service

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using Portfolio_Page.Models;
+using Portfolio_Page.Services;
+
+namespace Portfolio_Page.Controllers
+{
+    public class BlogController
+    {
+        private readonly BlogServices _service;
+
+        public BlogController(BlogServices service)
+        {
+            _service = service;
+        }
+
+        public async Task<List<BlogEntryModel>> GetAll() => 
+            await _service.GetBlogEntriesAsync("https://api.mortensens.xyz/kc/api/logs");
+
+    }
+}
