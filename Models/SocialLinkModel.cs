88a8519cc500f8fbb69b9f2abbfd476e3d3efe16
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-31 18:51:12 +0000

    Deploying to deploy from @ mttmortensen/Portfolio-Page@c031afcc073601742404bb6392242aea68ab0d53 🚀

--- Models/SocialLinkModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Portfolio_Page.Models
-{
-    public class SocialLinkModel
-    {
-        public string Platform { get; set; }
-        public string URL { get; set; }
-        public string Icon { get; set; } // Bootstrap Icons
-    }
-}
