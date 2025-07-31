384c6b22b29966f6f9eb9692454ee725ef0a5c80
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-31 09:14:37 -0600

    adding a service layer to blog

---
+++ Services/BlogServices.cs
@@ -0,0 +1,20 @@
+﻿using Portfolio_Page.Models;
+
+namespace Portfolio_Page.Services
+{
+    public class BlogServices
+    {
+        private readonly HttpClient _client;
+
+        public BlogServices(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<List<BlogEntryModel>> GetBlogEntriesAsync() 
+        {
+            // Logic here soon
+        }
+
+    }
+}
