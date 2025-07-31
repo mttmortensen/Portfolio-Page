330e8d7a1befe2680966408ed16b4293dc43207e
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-31 11:08:41 -0600

    empty service layer for SLs

---
+++ Services/Core/SocialLinkServices.cs
@@ -0,0 +1,12 @@
+﻿namespace Portfolio_Page.Services.Core
+{
+    public class SocialLinkServices
+    {
+        private readonly HttpClient _client;
+
+        public SocialLinkServices(HttpClient client)
+        {
+            _client = client;
+        }
+    }
+}
